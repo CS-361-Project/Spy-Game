df43eaec42d1a0f491246291eee22ca550b3966b
Sam Grund <samgrund@ip-65-65.wireless.oberlin.edu>
2016-04-04 20:17:52 -0400

    frank starting to wander

--- Assets/Resources/Scripts/Frank.cs
+++ Assets/Resources/Scripts/Frank.cs
@@ -2,23 +2,32 @@
 using System.Collections;
 
 public class Frank : MonoBehaviour {
-	
+	SpriteRenderer rend;
 	bool onFire;
 	enum states {smoking, drinking, talking, puking, shooting, running};
 
+	Vector2 position;
 	Vector2 direction;
 	Vector2 lineOfSight;
+	float speed;
 
 	Tile currentTile;
 
 
 	// Use this for initialization
-	void Start () {
+	public void init (Tile t, GameManager man) {
+		rend = gameObject.AddComponent<SpriteRenderer>();
+		rend.sprite = Resources.Load<Sprite>("Sprites/Guard");
+		rend.color = Color.magenta;
+		rend.sortingOrder = 1;
 		onFire = false;
+
 		gameObject.AddComponent<BoxCollider2D> ();
-		currentTile = GameManager.getClosestTile (transform.position);
+		currentTile = t;
+		position = currentTile.transform.position;
 		direction = new Vector2 (1f, 1f, 0f);
 		lineOfSight = direction;
+
 	}
 
 	void Wander(){
